0fa86fa06a68a73992799557cf0e262ad0bbb71e
DanieleMelo <dani.aparecida.ms@gmail.com>
2018-05-31 17:38:28 -0300

    Add project

---
+++ Do-it/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Do_it.MvcApplication" Language="C#" %>
---
+++ Do-it/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Do_it.Startup))]
+namespace Do_it
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
